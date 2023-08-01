1d544d407baf3f2a346c8ac3285c299c29540b9d
binbashz <5732@holbertonstudents.com>
2023-08-01 08:03:59 -0700

    GRANT ALL ON

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,11 @@
 --The script sets up a MySQL server for the AirBnB project, creating a database, a new user, and granting specific privileges.
 
 -- Create the hbnb_dev_db database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE database IF NOT EXISTS hbnb_dev_db;
 
 -- Create the hbnb_dev user if it doesn't exist and set the password to 'hbnb_dev_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant all privileges on hbnb_dev_db to the hbnb_dev user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege on performance_schema to the hbnb_dev user
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Grants the user  permisions
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';

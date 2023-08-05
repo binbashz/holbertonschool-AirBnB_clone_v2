2b9f6fc16dcbd88d41504755b0312403860b741f
RoPallas <124267669+RoPallas@users.noreply.github.com>
2023-08-05 16:54:36 -0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -42,11 +42,12 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = dict(self.__dict__)
-        if '_sa_instance_state' in dictionary:
-            del dictionary['_sa_instance_state']
+        dictionary = {}
+        dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary

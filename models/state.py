fb333220d0aa3c031665754afe5d4d13457d3c80
RoPallas <124267669+RoPallas@users.noreply.github.com>
2023-08-05 17:45:37 -0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -3,21 +3,26 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+import os
+
+env = os.getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    if env == 'db':
+        name = Column(String(128), nullable=False)
 
-
-    cities = relationship('City', back_populates='state', cascade='all, delete-orphan')
-
-    @property
-    def cities(self):
-        from models import storage
-        city_list = []
-        for city in storage.all('City').values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+        cities = relationship('City', back_populates='state', cascade='all, delete-orphan')
+    else:
+        name = ""
+        @property
+        def cities(self):
+            from models import storage
+            from models.city import City
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(storage.all(City)[city])
+            return city_list

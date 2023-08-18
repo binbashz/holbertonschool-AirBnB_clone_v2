679032086bc7c792f4dee3ecf78bc62598e6419a
binbashz <5732@holbertonstudents.com>
2023-08-18 07:59:35 -0700

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Flask Web Application"""
+
+# Import the Flask module
+from flask import Flask
+
+# Create a Flask web application instance
+app = Flask(__name__)
+
+# Define a route for the root URL ("/") and allow trailing slashes
+
+
+@app.route('/', strict_slashes=False)
+def say_hello():
+    """Display a warm greeting"""
+    return "Hello HBNB!"
+
+# Define a route for "/hbnb" URL and allow trailing slashes
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Display the magical word"""
+    return "HBNB"
+
+# Define a route for "/c/<text>" URL and allow trailing slashes
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """Display 'C' and the given text"""
+    formatted_text = text.replace("_", " ")
+    return "C " + formatted_text
+
+# Define a route for "/python" URL with a default value for 'text'
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+# Define a route for "/python/<text>" URL and allow trailing slashes
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text):
+    """Display 'Python' and the given text or a cool statement"""
+    formatted_text = text.replace("_", " ")
+    return "Python " + formatted_text
+
+# Define a route for "/number/<n>" URL and allow trailing slashes
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """Display n is a number if n is an integer"""
+    return f"{n} is a number"
+
+# Run the web application when this script is executed
+
+
+if __name__ == "__main__":
+    # Listen on the specified IP address and port (0.0.0.0:5000)
+    app.run(host='0.0.0.0', port=5000)

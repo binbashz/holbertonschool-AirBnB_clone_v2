191e8316c61f2b227e6c77e048921f527683f966
binbashz <5732@holbertonstudents.com>
2023-08-18 07:54:40 -0700

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-
+"""Task 0. Script that starts a Flask web application
+"""
 from flask import Flask
 
 # Create a Flask web application instance
 app = Flask(__name__)
 
-# Define a route for the root URL ("/") and allow trailing slashes
 
-
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
-
+    return 'Hello HBNB!'
 
 # Run the web application when this script is executed
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     # Listen on the specified IP address and port (0.0.0.0:5000)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)

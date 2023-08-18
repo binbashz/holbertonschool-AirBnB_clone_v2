b786e565440788f7cea6c9d072dbc33dac4efae5
binbashz <5732@holbertonstudents.com>
2023-08-18 09:01:44 -0700

    7-states_list.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,68 +1,48 @@
 #!/usr/bin/python3
-"""Flask Web Application"""
-
-# Import the Flask and render_template modules
+""" first flask web app """
 from flask import Flask, render_template
-from markupsafe import escape
-app = Flask(__name__)
 
-# Define a route for the root URL ("/") and allow trailing slashes
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def say_hello():
-    """Display a warm greeting"""
+def hello_hbnb():
+    """ returns Hello HBNB """
     return "Hello HBNB!"
-
-# Define a route for "/hbnb" URL and allow trailing slashes
 
 
 @app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """Display the magical word"""
+def hbnb():
+    """ Returns HBNB """
     return "HBNB"
-
-# Define a route for "/c/<text>" URL and allow trailing slashes
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c_text(text):
-    """Display 'C' and the given text"""
-    formatted_text = text.replace("_", " ")
-    return "C " + formatted_text
-
-# Define a route for "/python" URL with a default value for 'text'
-
-
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-# Define a route for "/python/<text>" URL and allow trailing slashes
+def c(text):
+    """ returns a text """
+    text_no_underscore = text.replace("_", " ")
+    return "C {}".format(text_no_underscore)
 
 
 @app.route('/python/<text>', strict_slashes=False)
-def display_python_text(text):
-    """Display 'Python' and the given text or a cool statement"""
-    formatted_text = text.replace("_", " ")
-    return "Python " + formatted_text
-
-# Define a route for "/number/<n>" URL and allow trailing slashes
+@app.route('/python', strict_slashes=False)
+def python(text="is_cool"):
+    """ freturns a text or a default value """
+    text_no_underscore = text.replace("_", " ")
+    return "Python {}".format(text_no_underscore)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
-    """Display n is a number if n is an integer"""
-    return f"{n} is a number"
-
-# Define a route for "/number_template/<n>" URL and allow trailing slashes
+def n(n):
+    """ prints a number """
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def display_number_template(n):
-    """Display a template with n if n is an integer"""
-    return render_template('custom_template.html', n=n)
-
-# Run the web application when this script is executed
+def num_temp(n):
+    """ renders a template with a given number """
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
-    # Listen on the specified IP address and port (0.0.0.0:5000)
     app.run(host='0.0.0.0', port=5000)
